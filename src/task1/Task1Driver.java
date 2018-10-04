a6ff54c4df8527b9e1d72b82f9dbc7458cf454e0
Ben <ppj1483@autuni.ac.nz>
2018-10-04 14:02:41 +1300

    Drivers

---
+++ src/task1/Task1Driver.java
@@ -0,0 +1,9 @@
+package task1;
+
+public class Task1Driver
+{
+	public static void main(String[] args)
+	{
+	
+	}
+}
---
+++ src/task2/Task2Driver.java
@@ -0,0 +1,9 @@
+package task2;
+
+public class Task2Driver
+{
+	public static void main(String[] args)
+	{
+	
+	}
+}
